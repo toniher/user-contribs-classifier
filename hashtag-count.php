09732a37da09aa083d007c0960d6e7a3dc75b9e0
Toni Hermoso Pulido <toniher@cau.cat>
2019-03-16 17:52:19 +0100

    more going on

--- hashtag-count.php
+++ hashtag-count.php
@@ -76,7 +76,7 @@ if ( array_key_exists( "tag", $props )  &&  array_key_exists( "startdate", $prop
 	
 	#https://ca.wikipedia.org/w/api.php?action=query&list=recentchanges&rcprop=comment|user|title|timestamp|sizes|redirect
 	
-	$params = array( "list" => "recentchanges", "rcprop" => "comment|title", "rclimit" => $rclimit, "rcdir" => "newer", "rcnamespace" => 0 );
+	$params = array( "list" => "recentchanges", "rcprop" => "comment|title|user", "rclimit" => $rclimit, "rcdir" => "newer", "rcnamespace" => 0 );
 	
 	if ( array_key_exists( "startdate", $props ) ) {
 		$params["rcstart"] = $props["startdate"];
@@ -90,7 +90,9 @@ if ( array_key_exists( "tag", $props )  &&  array_key_exists( "startdate", $prop
 	}
 	
 	$pages = retrieveWpQuery( $pages, $wpapi, $params, null, $props );
-	print_r( array_keys( $pages ) );
+	var_dump( $pages );
+	
+	$history = retrieveHistoryPages( $pages, $wpapi, $props );
 
 }
 
@@ -132,6 +134,89 @@ function retrieveWpQuery( $pages, $wpapi, $params, $uccontinue, $props ) {
 	
 }
 
+function retrieveHistoryPages( $pages, $wpapi, $props ) {
+	
+	$history = array();
+	$batch = 5; // Batch to query, less API requests
+	$stack = array( );
+	
+	$rvlimit = 500;
+	$params = array( "prop" => "revisions", "rvlimit" => $rvlimit, "rvdir" => "newer", "rvprop" => "user|size|ids" );
+	
+	
+	if ( array_key_exists( "startdate", $props ) ) {
+		$params["rvstart"] = $props["startdate"];
+	}
+	if ( array_key_exists( "enddate", $props ) ) {
+		$params["rvend"] = $props["enddate"];
+	}
+	
+	$s = 0;
+	
+	foreach( array_keys( $pages ) as $page ) {
+		
+		$params["titles"] = $page;
+		$userContribRequest = new Mwapi\SimpleRequest( 'query', $params  );
+		$outcome = $wpapi->postRequest( $userContribRequest );
+	
+		$history = processHistory( $history, $wpapi, $outcome );
+
+	}
+	
+	return $history;
+	
+}
+
+function processHistory( $history, $wpapi, $outcome ) {
+
+
+	if ( array_key_exists( "query", $outcome ) ) {
+	
+		if ( array_key_exists( "pages", $outcome["query"] ) ) {
+
+			foreach ( $outcome["query"]["pages"] as $page )  {
+			
+				$title = $page["title"];
+				
+				$history[$title] = array();
+				$history[$title]["parentrev"] = null;
+				$history[$title]["contribs"] = array();
+				$history[$title]["created"] = false;
+				$parentid = null;
+				$parentsize = 0;
+				$parentCategories = array();
+				
+				if ( array_key_exists( "revisions", $page ) ) {
+					
+					$revisions = $page["revisions"];
+					
+					if ( count( $revisions ) > 0 ) {
+						
+						if ( array_key_exists( "parentid", $revisions[0] ) ) {
+							
+							$parentid = $revisions[0]["parentid"];
+							
+							# If page not created, proceed
+							if ( $parentid > 0 ) {
+								#https://ca.wikipedia.org/w/api.php?action=query&revids=20872217&rvprop=size|user|timestamp&prop=revisions|categories
+							}
+							
+						}
+						
+					}
+					
+				}
+				
+			}
+	
+		}
+	
+	}
+
+	return $history;
+}
+
+
 
 function processPages( $pages, $contribs, $props ) {
 	
@@ -139,10 +224,15 @@ function processPages( $pages, $contribs, $props ) {
 		
 		$title = strval( $contrib["title"] );
 		$comment = strval( $contrib["comment"] );
+		$user = strval( $contrib["user"] );
 
 		if ( detectTag( $comment, $props["tag"] ) ) {
 	
-			$pages[$title] = true;
+			if ( array_key_exists( $title, $pages ) ) {
+				array_push( $pages[$title], $user );
+			} else {
+				$pages[$title] = array( $user );
+			}
 		}
 	}
 	return $pages;
